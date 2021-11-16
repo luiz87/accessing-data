ea405782c5f76e41d8d27f9562c2766f8e3a28db
luiz-felipe <luizfelipe.freire@gmail.com>
2021-11-16 20:17:40 -0300

    Implentadado os método PUT e DELETE para atualizar e apagar registros de usuários.

--- src/main/java/com/example/accessingdata/UsuarioRepository.java
+++ src/main/java/com/example/accessingdata/UsuarioRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
 
-    
+    public Usuario findById(int id);
 }
